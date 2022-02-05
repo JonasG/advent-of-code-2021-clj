49a3794d99236ce4bb4ab4e30a8d41008f7266df
jonasgranqvist <jonasgranqvist@penguin.lxd>
2022-02-05 19:22:29 +0100

    Day 4 part 1 solution

--- src/advent_of_code_2021_clj/core.clj
+++ src/advent_of_code_2021_clj/core.clj
@@ -34,72 +34,10 @@
 (defn every-marked? [coll] (every? is-marked? coll))
 (defn some-row-marked? [board] (some every-marked? board))
 (defn some-column-marked? [board] (some-row-marked? (transpose board)))
-
-(some-column-marked? [[ 1  2  3  4  5]
-                 [ 6  7  8  9 10]
-                 [11 12 13 14 15]
-                 [16 17 18 19 20]
-                 [21 22 23 24 25]])
-
-(some-column-marked? [[ "X"  2  3  4  5]
-                 [ "X"  7  8  9 10]
-                 ["X" 12 13 14 15]
-                 ["X" 17 18 19 20]
-                 ["X" 22 23 24 25]])
-
-(some-column-marked? [[ 1  "X"  3  4  5]
-                 [ 6  "X"  8  9 10]
-                 [11 "X" 13 14 15]
-                 [16 "X" 18 19 20]
-                 [21 "X" 23 24 25]])
-
-(some-column-marked? [[ 1  2  "X"  4  5]
-                 [ 6  7  "X"  9 10]
-                 [11 12 "X" 14 15]
-                 [16 17 "X" 19 20]
-                 [21 22 "X" 24 25]])
-
-(some-column-marked? [[ 1  2  3  "X"  5]
-                 [ 6  7  8  "X" 10]
-                 [11 12 13 "X" 15]
-                 [16 17 18 "X" 20]
-                 [21 22 23 "X" 25]])
-
-(some-column-marked? [[ 1  2  3  4 "X"]
-                 [ 6  7  8  9 "X"]
-                 [11 12 13 14 "X"]
-                 [16 17 18 19 "X"]
-                 [21 22 23 24 "X"]])
-
-(some-row-marked? [[ 1  2  3  4  5]
-              [ 6  7  8  9 10]
-              [11 12 13 14 15]
-              [16 17 18 19 20]
-              [21 22 23 24 25]])
-(some-row-marked? [[  1   2   3   4   5]
-              ["X" "X" "X" "X" "X"]
-              [ 11  12  13  14  15]
-              [ 16  17  18  19  20]
-              [ 21  22  23  24  25]])
-
-(def example-data [[ 1  2  3  4  5]
-                 [ 6  7  8  9 "X"]
-                 [11 12 13 14 "X"]
-                 [16 17 18 19 "X"]
-                 [21 22 23 24 "X"]])
-
-(defn board-won? [board] (or (some-row-marked? board) (some-column-marked? board)))
-
-(board-won? [[ 1  2  3  4 5]
-                 [ 6  7  8  9 10]
-                 [11 12 13 14 10]
-                 [16 17 18 19 10]
-                 ["X" "X" 5 "X" "X"]])
-(board-won? [[ 1  2  3  4 "X"]
-                 [ 6  7  8  9 "X"]
-                 ["X" "X" "X" "X" "X"]
-                 [16 17 18 19 "X"]
-                 [21 22 23 24 "X"]])
+(defn board-won? [board]
+  (if (or (some-row-marked? board) (some-column-marked? board))
+    board
+    false))
 
 (defn iterate-2d-vector-filtered [board filter]
   (for [[_ row] (map-indexed list board)
@@ -107,22 +45,33 @@
         :when (filter value)]
     value))
 
-(iterate-2d-vector-filtered [[1 2]
-                             ["X" 4]] #(not (= "X" %)))
-
 (defn sum [coll] (reduce + coll))
-
 (defn score [board winning-number]
   (* winning-number (sum (iterate-2d-vector-filtered board #(not (is-marked? %))))))
 
-(score [[1 2]
-        ["X" 4]] 42)
+(defn mark-board [board number-drawn]
+  (mapv #(replace {number-drawn "X"} %) board))
+
+(defn play-bingo [boards draws]
+  (reduce (fn [board-state number-drawn]
+            (let [new-board-state (mapv #(mark-board % number-drawn) board-state)]
+              (if (some board-won? new-board-state)
+                (reduced {:winning-board-state (some board-won? new-board-state) :number number-drawn})
+                new-board-state)))
+          boards
+          draws))
+
+(defn day4-part1 [input]
+  (let [result (play-bingo (:boards input) (:numbers input))]
+     (score (:winning-board-state result) (:number result))))
 
 (defn parse-input [input-str] 
   (let [boards (read-boards input-str)
         lines (str/split input-str #"\n")
         numbers (read-numbers (first (take 1 lines)))]
     {:numbers numbers :boards boards}))
+
+(day4-part1 (parse-input (read-input-file "day4.txt")))
 
 (defn -main
   [& args]
