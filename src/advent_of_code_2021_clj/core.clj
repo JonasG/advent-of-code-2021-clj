1b3186e6a17ee9c433f428410620d2cbec5bf9e1
jonasgranqvist <jonasgranqvist@penguin.lxd>
2022-02-05 16:45:33 +0100

    Improvements and simplifications

--- src/advent_of_code_2021_clj/core.clj
+++ src/advent_of_code_2021_clj/core.clj
@@ -27,21 +27,56 @@
         boards (mapv #((comp read-single-board (partial str/join \newline)) %) board-row-strings)]
     boards))
 
-(defn mark-number-on-board [number board]
-  (mapv (partial replace {number "X"}) board))
+(defn transpose [matrix]
+  (apply mapv vector matrix))
 
-(defn mark-number-on-boards [number boards]
-  (mapv (partial mark-number-on-board number) boards))
+(defn is-marked? [value] (= "X" value))
+(defn every-marked? [coll] (every? is-marked? coll))
+(defn some-row-marked? [board] (some every-marked? board))
+(defn some-column-marked? [board] (some-row-marked? (transpose board)))
 
-(defn all-marked? [coll] (every? #(= "X" %) coll))
-(defn won-by-row? [board] (some all-marked? board))
+(some-column-marked? [[ 1  2  3  4  5]
+                 [ 6  7  8  9 10]
+                 [11 12 13 14 15]
+                 [16 17 18 19 20]
+                 [21 22 23 24 25]])
 
-(won-by-row? [[ 1  2  3  4  5]
+(some-column-marked? [[ "X"  2  3  4  5]
+                 [ "X"  7  8  9 10]
+                 ["X" 12 13 14 15]
+                 ["X" 17 18 19 20]
+                 ["X" 22 23 24 25]])
+
+(some-column-marked? [[ 1  "X"  3  4  5]
+                 [ 6  "X"  8  9 10]
+                 [11 "X" 13 14 15]
+                 [16 "X" 18 19 20]
+                 [21 "X" 23 24 25]])
+
+(some-column-marked? [[ 1  2  "X"  4  5]
+                 [ 6  7  "X"  9 10]
+                 [11 12 "X" 14 15]
+                 [16 17 "X" 19 20]
+                 [21 22 "X" 24 25]])
+
+(some-column-marked? [[ 1  2  3  "X"  5]
+                 [ 6  7  8  "X" 10]
+                 [11 12 13 "X" 15]
+                 [16 17 18 "X" 20]
+                 [21 22 23 "X" 25]])
+
+(some-column-marked? [[ 1  2  3  4 "X"]
+                 [ 6  7  8  9 "X"]
+                 [11 12 13 14 "X"]
+                 [16 17 18 19 "X"]
+                 [21 22 23 24 "X"]])
+
+(some-row-marked? [[ 1  2  3  4  5]
               [ 6  7  8  9 10]
               [11 12 13 14 15]
               [16 17 18 19 20]
               [21 22 23 24 25]])
-(won-by-row? [[  1   2   3   4   5]
+(some-row-marked? [[  1   2   3   4   5]
               ["X" "X" "X" "X" "X"]
               [ 11  12  13  14  15]
               [ 16  17  18  19  20]
@@ -53,68 +88,35 @@
                  [16 17 18 19 "X"]
                  [21 22 23 24 "X"]])
 
-(defn won-by-column? [board]
-  (or (all-marked? [(get-in board [0 0])
-                    (get-in board [1 0])
-                    (get-in board [2 0])
-                    (get-in board [3 0])
-                    (get-in board [4 0])])
-      (all-marked? [(get-in board [0 1])
-                    (get-in board [1 1])
-                    (get-in board [2 1])
-                    (get-in board [3 1])
-                    (get-in board [4 1])])
-      (all-marked? [(get-in board [0 2])
-                    (get-in board [1 2])
-                    (get-in board [2 2])
-                    (get-in board [3 2])
-                    (get-in board [4 2])])
-      (all-marked? [(get-in board [0 3])
-                    (get-in board [1 3])
-                    (get-in board [2 3])
-                    (get-in board [3 3])
-                    (get-in board [4 3])])
-      (all-marked? [(get-in board [0 4])
-                    (get-in board [1 4])
-                    (get-in board [2 4])
-                    (get-in board [3 4])
-                    (get-in board [4 4])])))
+(defn board-won? [board] (or (some-row-marked? board) (some-column-marked? board)))
 
-(won-by-column? [[ 1  2  3  4  5]
+(board-won? [[ 1  2  3  4 5]
                  [ 6  7  8  9 10]
-                 [11 12 13 14 15]
-                 [16 17 18 19 20]
-                 [21 22 23 24 25]])
-
-(won-by-column? [[ "X"  2  3  4  5]
-                 [ "X"  7  8  9 10]
-                 ["X" 12 13 14 15]
-                 ["X" 17 18 19 20]
-                 ["X" 22 23 24 25]])
-
-(won-by-column? [[ 1  "X"  3  4  5]
-                 [ 6  "X"  8  9 10]
-                 [11 "X" 13 14 15]
-                 [16 "X" 18 19 20]
-                 [21 "X" 23 24 25]])
-
-(won-by-column? [[ 1  2  "X"  4  5]
-                 [ 6  7  "X"  9 10]
-                 [11 12 "X" 14 15]
-                 [16 17 "X" 19 20]
-                 [21 22 "X" 24 25]])
-
-(won-by-column? [[ 1  2  3  "X"  5]
-                 [ 6  7  8  "X" 10]
-                 [11 12 13 "X" 15]
-                 [16 17 18 "X" 20]
-                 [21 22 23 "X" 25]])
-
-(won-by-column? [[ 1  2  3  4 "X"]
+                 [11 12 13 14 10]
+                 [16 17 18 19 10]
+                 ["X" "X" 5 "X" "X"]])
+(board-won? [[ 1  2  3  4 "X"]
                  [ 6  7  8  9 "X"]
-                 [11 12 13 14 "X"]
+                 ["X" "X" "X" "X" "X"]
                  [16 17 18 19 "X"]
                  [21 22 23 24 "X"]])
+
+(defn iterate-2d-vector-filtered [board filter]
+  (for [[_ row] (map-indexed list board)
+        [_ value] (map-indexed list row)
+        :when (filter value)]
+    value))
+
+(iterate-2d-vector-filtered [[1 2]
+                             ["X" 4]] #(not (= "X" %)))
+
+(defn sum [coll] (reduce + coll))
+
+(defn score [board winning-number]
+  (* winning-number (sum (iterate-2d-vector-filtered board #(not (is-marked? %))))))
+
+(score [[1 2]
+        ["X" 4]] 42)
 
 (defn parse-input [input-str] 
   (let [boards (read-boards input-str)
