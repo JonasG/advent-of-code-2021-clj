9e51fc39c07c0e9335e6483e4e7956242a07c1a0
jonasgranqvist <jonasgranqvist@penguin.lxd>
2022-02-07 21:46:50 +0100

    Threading operator

--- src/advent_of_code_2021_clj/day8.clj
+++ src/advent_of_code_2021_clj/day8.clj
@@ -18,10 +18,13 @@
                8 (first (segment-count 7 as-sets))} m
       (assoc m 3 (first (find-supersets-of (get m 1) (segment-count 5 as-sets))))
       (assoc m 9 (first (find-supersets-of (get m 3) (segment-count 6 as-sets))))
-      (assoc m 0 (first (remove #(= (get m 9) %) (find-supersets-of (get m 1) (segment-count 6 as-sets)))))
-      (assoc m 6 (first (remove #(= (get m 9) %) (remove #(= (get m 0) %) (segment-count 6 as-sets)))))
+      (assoc m 0 (first (->> as-sets
+                             (segment-count 6)
+                             (find-supersets-of (get m 1))
+                             (remove (get m 9)))))
+      (assoc m 6 (first (remove (get m 9) (remove (get m 0) (segment-count 6 as-sets)))))
       (assoc m 5 (first (filter #(clojure.set/subset? % (get m 6)) (segment-count 5 as-sets))))
-      (assoc m 2 (first (remove #(= (get m 3) %) (remove #(= (get m 5) %) (segment-count 5 as-sets))))))))
+      (assoc m 2 (first (remove (get m 3) (remove (get m 5) (segment-count 5 as-sets))))))))
 
 (number-to-segment-mapping '("acedgfb" "cdfbe" "gcdfa" "fbcad" "dab" "cefabd" "cdfgeb" "eafb" "cagedb" "ab"))
 
